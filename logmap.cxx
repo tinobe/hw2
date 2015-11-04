7256af454a3eea4bb08c7d2aff2085906e340a59
Götz <goetz@tp1.uni-duesseldorf.de>
2015-11-04 11:46:56 +0100

    Update logmap.cxx

--- logmap.cxx
+++ logmap.cxx
@@ -10,9 +10,8 @@ int main(){
 
 	for(double r=0; r <= 4; r += 0.001){
 	   x=x0;
-	   for(int i=1; i <= Nskip; i++){
+	   for(int i=1; i <= Nskip; i++)
 		   x = r*x*(1-x);
-	   }
 	   for(int i=Nskip+1; i <= Nend; i++){
 	   		   x = r*x*(1-x);
 	   		   cout << r << "\t" << x << endl;
