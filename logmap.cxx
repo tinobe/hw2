c156e390b0456f5ac0aafb8f4eb147242a2f689f
Konstantin Beyer <kobey100@pc04.pool.tp1.uni-duesseldorf.de>
2015-11-10 19:36:06 +0100

    The values of x are saved to an array instead of directly streamed.

--- logmap.cxx
+++ logmap.cxx
@@ -1,23 +1,27 @@
 #include <iostream>
+#include<fstream>
 
 using namespace std;
 
 int main(){
-	double x0 = 0.5;
-	double x;
+	double x0 = 0.5,xskiped;
 	int Nskip = 100; //Number of iterations to skip
 	int Nend  = 200; //Number of total iterations
+	double x[Nend-Nskip];
+ofstream out("logmap-data.dat");
 
 	for(double r=0; r <= 4; r += 0.001){
-	   x=x0;
-	   for(int i=1; i <= Nskip; i++)
-		   x = r*x*(1-x);
-	   for(int i=Nskip+1; i <= Nend; i++){
-	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
-   	   }
+		xskiped=x0;
+	   for(int i=1; i <= Nskip+1; i++)
+		   xskiped = r*xskiped*(1-xskiped);
+		x[0]=xskiped;
+	   for(int i=Nskip+2; i <= Nend; i++)
+	   	   x[i-Nskip-1] = r*x[i-Nskip-2]*(1-x[i-Nskip-2]);
+	   for(int i=0;i<=Nend-Nskip-1;i++){
+		   out << r << "\t" << x[i] << endl;
+		}
 	}
 
-
+out.close();
 	return 0;
 }
